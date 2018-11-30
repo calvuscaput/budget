6f7b6eef78034460fd03e6d04099e7bb68f3444d
CalvusCaput <CalvusCaput@yandex.ru>
2018-11-30 13:03:47 +0300

    Updates

---
+++ precache-manifest.52d76295e67db247feceae606ab88e40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1adf0441c270d5a70447",
+    "url": "/budget/static/js/runtime~main.1adf0441.js"
+  },
+  {
+    "revision": "baa1573dac8a6c75b086",
+    "url": "/budget/static/js/main.baa1573d.chunk.js"
+  },
+  {
+    "revision": "3585b0ec69ac13a780d0",
+    "url": "/budget/static/js/1.3585b0ec.chunk.js"
+  },
+  {
+    "revision": "baa1573dac8a6c75b086",
+    "url": "/budget/static/css/main.1d1db30a.chunk.css"
+  },
+  {
+    "revision": "89f9175991273514a3134dbe842a7861",
+    "url": "/budget/index.html"
+  }
+];
